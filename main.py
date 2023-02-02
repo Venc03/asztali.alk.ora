e9dd912bda8bdba504f93917879132ed3e0d8a0f
Venc03 <vegi.dani1@gmail.com>
2023-02-02 13:24:49 +0100

    +testeredmenyek(8)

--- main.py
+++ main.py
@@ -21,8 +21,28 @@ def osszegzes(list: list[int]):
 
 
 def jatekos_vesztett_teszt():
-    jLap = [10, 5, 7]
-    gLap = [2, 7, 9]
+    jLap = [10, 12]
+    gLap = [10, 8]
+    kap = eredmeny(jLap, gLap)
+    vart = "Játékos vesztett"
+    if kap == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
+
+def jatekos_vesztett_teszt_2():
+    jLap = [11, 5, 2]
+    gLap = [9, 10]
+    kap = eredmeny(jLap, gLap)
+    vart = "Játékos vesztett"
+    if kap == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
+
+def jatekos_vesztett_teszt_3():
+    jLap = [5, 3, 5]
+    gLap = [9, 5, 6]
     kap = eredmeny(jLap, gLap)
     vart = "Játékos vesztett"
     if kap == vart:
@@ -32,8 +52,8 @@ def jatekos_vesztett_teszt():
 
 
 def gep_vesztett_teszt():
-    jLap = [10, 5, 4]
-    gLap = [6, 7, 10]
+    jLap = [10, 11]
+    gLap = [10, 10]
     kap = eredmeny(jLap, gLap)
     vart = "Gép vesztett"
     if kap == vart:
@@ -42,6 +62,25 @@ def gep_vesztett_teszt():
         print("A teszt megbukott")
 
 
+def gep_vesztett_teszt_2():
+    jLap = [7, 10, 4]
+    gLap = [11, 8]
+    kap = eredmeny(jLap, gLap)
+    vart = "Gép vesztett"
+    if kap == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
+
+def gep_vesztett_teszt_3():
+    jLap = [10, 5, 6]
+    gLap = [5, 3, 5]
+    kap = eredmeny(jLap, gLap)
+    vart = "Gép vesztett"
+    if kap == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
 
 def dontetlen_teszt():
     jLap = [10, 5, 7]
@@ -52,10 +91,24 @@ def dontetlen_teszt():
     else:
         print("A teszt megbukott")
 
+def dontetlen_teszt_2():
+    jLap = [10, 7, 6]
+    gLap = [11, 11]
+
+    if osszegzes(jLap) == osszegzes(gLap):
+        print("A teszt sikeres")
+    else:
+        print("A teszt megbukott")
+
 def tesztek():
     jatekos_vesztett_teszt()
+    jatekos_vesztett_teszt_2()
+    jatekos_vesztett_teszt_3()
     gep_vesztett_teszt()
+    gep_vesztett_teszt_2()
+    gep_vesztett_teszt_3()
     dontetlen_teszt()
+    dontetlen_teszt_2()
 
 
 tesztek()
